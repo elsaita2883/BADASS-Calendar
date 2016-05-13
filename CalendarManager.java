13dd9b73210299ee37115681748de7662b842735
PC3000 <jasonp1998@gmail.com>
2016-05-13 09:24:42 -0500

    removing search stuff

---
+++ CalendarManager.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of class CalendarManager here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class CalendarManager
+{
+    public static Period curFirstPeriod;
+    //sets curFirstPeiod to first period of the first day of the current week
+    public static void init(){
+        
+    }
+    //moves to the next week
+    public static void next(){
+        
+    }
+    //move to the past week
+    public static void prev(){
+    
+    }
+}
