35c84c06fcdcbc3b729dc671fecb5b30a9e0b44e
PC3000 <jasonp1998@gmail.com>
2016-05-14 17:52:39 -0500

    fixed week class

--- JasonTester.java
+++ JasonTester.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 import java.io.*;
 /**
  * Write a description of class Tester here.
@@ -9,37 +9,10 @@ import java.io.*;
 public class JasonTester
 {
     public static void main(String args[])throws Exception{
-        Period pr = new Period(2016,5,9,3);
-        if(args[0]=="save"){
-            Reservation rs = new Reservation("Byron Webb", "AP Computer Science");
-            
-            HashMap<Period,Reservation> map= new HashMap<Period,Reservation>();
-            map.put(pr,rs);
-            
-            File fl = new File("mapstorage.ser");
-            fl.createNewFile();
-            
-            boolean get = map.containsKey(pr); //get the data corresponding to the key
-            System.out.println(get);
-            
-            System.out.println(map);
-            
-            DatabaseManager.saveReservations(fl,map);
-            
-        }else if(args[0]=="load"){
-            File fl = new File("mapstorage.ser");
-            HashMap<Period,Reservation> map2= DatabaseManager.loadReservations(fl);
-            Object[] arr = map2.keySet().toArray();
-            
-            System.out.println(pr.equals((Period)arr[0]));
-            
-            System.out.println(pr.hashCode());
-            System.out.println(arr[0].hashCode());
-            
-            System.out.println(map2.get(pr));
-            System.out.println(map2.get(arr[0]));
-            
-            System.out.println(map2);
-        }
+        Week week = new Week();
+        Period[][] pr = week.getPeriods();
+        for(int i = 0;i<pr.length;i++)
+            for(int c = 0;c < pr[i].length; c++)
+                System.out.println(pr[i][c]);
     }
 }
--- Week.java
+++ Week.java
@@ -7,13 +7,48 @@
 
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *
  * @author Owner
  */
 public class Week {
+    private GregorianCalendar[] days;
+    
+    //creates the current week
+    public Week(){
+        this.days  = new GregorianCalendar[5];
+        GregorianCalendar init = new GregorianCalendar();
+        //roll back to monday
+        while(init.get(Calendar.DAY_OF_WEEK)!=Calendar.MONDAY){
+            init.add(Calendar.DAY_OF_MONTH,-1);
+        }
+        //fill the array with consecutive days
+        for(int i = 0;i < days.length;i++){
+            days[i]=(GregorianCalendar)init.clone();
+            init.add(Calendar.DAY_OF_MONTH,1);
+        }
+    }
+    //returns an array of Period[day][period]
+    public Period[][] getPeriods() throws PeriodException{
+        Period[][] week = new Period[5][8];
+        for(int d = 0; d < days.length; d++){
+            for(int p = 0;p < 8;p++){
+                Calendar cur = days[d];
+                int year = cur.get(Calendar.YEAR);
+                int month = cur.get(Calendar.MONTH) + 1;
+                int day = cur.get(Calendar.DAY_OF_MONTH);
+                int period = p+1;
+                week[d][p] = new Period( year,month,day,period);
+            }
+        }
+        return week;
+    }
+    /*
     private final Day[] days = new Day[5];
+    
     public Week(Day firstDay){
         days[0] = firstDay;
         for (int i = 1; i < 5; i++){
@@ -24,6 +59,7 @@ public class Week {
     public Day getDay(int index){
         return days[index];
     }
+    */
     public String getDayName(int index){
         switch(index){
             case 2:
@@ -48,7 +84,7 @@ public class Week {
             g2.drawString(days[i].toString(),dayName.x + 20, dayName.y + 40);
         }
         for(int i = 0; i < 5; i++){
-            Day day = days[i];
+            Calendar day = days[i];
             for (int j = 0; j < 8; j++){
                 Rectangle period = new Rectangle(200 + 100*i, 250 + 50*j,100,50);
                 g2.draw(period);
