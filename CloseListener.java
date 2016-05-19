825319d05241dbbc5bf61ceb187914c90677f52b
PC3000 <jasonp1998@gmail.com>
2016-05-19 09:30:42 -0500

    added window listener and other junk sorry

--- CloseListener.java
+++ CloseListener.java
@@ -1,4 +1,5 @@
 import java.awt.event.*;
+import java.io.*;
 /**
  * Write a description of class WindowListener here.
  * 
@@ -17,7 +18,31 @@ public class CloseListener implements WindowListener
     }
 
     public void windowDeactivated(java.awt.event.WindowEvent e){
-        DatabaseManager.saveReservation(new File("map.ser"));
+        try{
+            DatabaseManager.saveReservations(new File("map.ser"));
+        }catch(Exception ex){
+            System.out.println("no file");
+        }
         System.exit(0);
     }
+    
+    public void windowActivated(java.awt.event.WindowEvent e){
+    }
+    
+    public void windowDeiconified(java.awt.event.WindowEvent e){
+    }
+    
+    public void windowIconified(java.awt.event.WindowEvent e){
+    }
+    
+    public void windowClosed(java.awt.event.WindowEvent e){
+    }
+    
+    public void windowClosing(java.awt.event.WindowEvent e){
+    }
+    
+    public void windowOpened(java.awt.event.WindowEvent e){
+    }
+    
+    
 }
