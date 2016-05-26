647351cb4890444f09d249442b625f578ac4e8c6
PC3000 <jasonp1998@gmail.com>
2016-05-25 21:34:10 -0500

    Update NewJFrame.java

--- NewJFrame.java
+++ NewJFrame.java
@@ -2,16 +2,10 @@ package badass.calendar;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import javax.swing.JCheckBox;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- *
- * @author Swetal
+ * Makes the calendar JFrame
+ * 
+ * @author Zeel Parikh
  */
 public class NewJFrame extends javax.swing.JFrame {
 
